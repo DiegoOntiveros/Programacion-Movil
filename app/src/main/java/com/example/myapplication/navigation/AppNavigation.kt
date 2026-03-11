d499c66e2c7bcc34102ca362f029746c8dbe2686
Diego Ontiveros <dontiveros_23@alu.uabcs.mx>
2026-03-11 13:24:07 -0700

    Refactor: separación de pantallas, navegación y corrección de temas

---
+++ app/src/main/java/com/example/myapplication/navigation/AppNavigation.kt
@@ -0,0 +1,20 @@
+package com.example.myapplication.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.myapplication.screens.WelcomeScreen
+import com.example.myapplication.screens.LoginScreen
+import com.example.myapplication.screens.SignUpScreen
+
+@Composable
+fun AppNavigation() {
+    val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = "welcome") {
+        composable("welcome") { WelcomeScreen(navController) }
+        composable("login") { LoginScreen(navController) }
+        composable("signup") { SignUpScreen(navController) }
+    }
+}
--- app/src/main/java/com/example/myapplication/ui/theme/Color.kt
+++ app/src/main/java/com/example/myapplication/ui/theme/Color.kt
@@ -2,10 +2,19 @@ package com.example.myapplication.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val Purple       = Color(0xFF3D3AA8)
+val White        = Color(0xFFFFFFFF)
+val TextDark     = Color(0xFF1A1A2E)
+val TextGray     = Color(0xFF8888AA)
+val LightGray    = Color(0xFFF5F5FA)
+val FacebookBlue = Color(0xFF3B5998)
+val GoogleRed    = Color(0xFFDB4437)
+val LinkedInBlue = Color(0xFF0077B5)
 
-val Purple40 = Color(0xFF6650a4)
+val Purple80     = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80       = Color(0xFFEFB8C8)
+
+val Purple40     = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Pink40       = Color(0xFF7D5260)
